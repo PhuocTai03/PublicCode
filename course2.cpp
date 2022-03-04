d9dea7f1d6cbe170c5f8a32b4416d9e525ee5ba5
Dao Phuoc Tai <87965099+PhuocTai03@users.noreply.github.com>
2022-03-04 15:26:03 +0700

    Update course2.cpp

--- course2.cpp
+++ course2.cpp
@@ -4,15 +4,21 @@
 #include <iomanip>
 #include <crtdefs.h>
 #define MAXN 500000
-#define max(a, b)  (a>b)?a:b
+// A utility function to find maximum of two integers
+int max(int a, int b) { return (a > b) ? a : b; }
+ 
+// A utility function to find maximum of three integers
+int max(int a, int b, int c) { return max(max(a, b), c); }
 using namespace std;
 void RandomArray(int A[], int n){
     srand (time(NULL));
     for(int i=0; i < n; i++)
         A[i]=(rand()%100+1) - 50;
 }
-int ThuatToan1(int A[], int n){
-    int maxSum = 0;
+float ThuatToan1(int A[], int n, int& maxSum){
+    clock_t start, end;
+    start = clock();
+    maxSum = INT_MIN;
     int sum = 0;
     for (int i = 0; i < n; i ++) {
         for (int j = i; j < n; j ++) {
@@ -23,54 +29,83 @@ int ThuatToan1(int A[], int n){
             maxSum = max(maxSum, sum);
         }
     }
-    return maxSum;
+    end = clock();
+    return (float)(end-start)/CLOCKS_PER_SEC;
 }
-int ThuatToan2(int A[], int n){
-    int maxSum = 0;
+float ThuatToan2(int A[], int n, int& maxSum){
+    clock_t start, end;
+    start = clock();
+    maxSum = INT_MIN;
+    int sum = 0;
     for( int i = 0; i < n; i++ ){
-        int sum = 0;
+        sum = 0;
         for( int j = i; j < n ; j++ ) {
             sum += A[j];
             maxSum = max(maxSum, sum);
         } 
     }
-    return maxSum;
+    end = clock();
+    return (float)(end-start)/CLOCKS_PER_SEC;
 }
-int maxCrossingSum(int arr[], int l, int mid, int r) {
-    // Include e lements on left of mid.
+// Find the maximum possible sum in arr[] auch that arr[m]
+// is part of it
+int maxCrossingSum(int arr[], int l, int m, int r)
+{
+    // Include elements on left of mid.
     int sum = 0;
     int left_sum = INT_MIN;
-    for (int i = mid; i >= l; i--) {
+    for (int i = m; i >= l; i--) {
         sum = sum + arr[i];
-        if (sum > left_sum) left_sum = sum;
+        if (sum > left_sum)
+            left_sum = sum;
     }
-     // Include elements on right of mid
+ 
+    // Include elements on right of mid
     sum = 0;
     int right_sum = INT_MIN;
-    for (int i = mid+1; i <= r; i++) {
+    for (int i = m + 1; i <= r; i++) {
         sum = sum + arr[i];
-        if (sum > right_sum) right_sum = sum;
-    } 
+        if (sum > right_sum)
+            right_sum = sum;
+    }
+ 
     // Return sum of elements on left and right of mid
-    return left_sum + right_sum;
+    // returning only left_sum + right_sum will fail for
+    // [-2, 1]
+    return max(left_sum + right_sum, left_sum, right_sum);
 }
-int maxSubArraySum(int arr[], int l, int r) {
-   // Base Case: Only one element
-   if (l == r) return arr[l];
-   int mid = (l + r)/2;
+ 
+// Returns sum of maximum sum subarray in aa[l..r]
+int maxSubArraySum(int arr[], int l, int r)
+{
+    // Base Case: Only one element
+    if (l == r)
+        return arr[l];
+ 
+    // Find middle point
+    int m = (l + r) / 2;
+ 
     /* Return maximum of following three possible cases
-      a) Maximum subarray sum in left half
-      b) Maximum subarray sum in right half
-      c) Maximum subarray sum such that the subarray crosses the midpoint */
-   return max(max(maxSubArraySum(arr, l, mid),maxSubArraySum(arr, mid+1, r)),
-                            maxCrossingSum(arr, l, mid, r));
+            a) Maximum subarray sum in left half
+            b) Maximum subarray sum in right half
+            c) Maximum subarray sum such that the subarray
+       crosses the midpoint */
+    return max(maxSubArraySum(arr, l, m),
+               maxSubArraySum(arr, m + 1, r),
+               maxCrossingSum(arr, l, m, r));
 }
-int ThuatToan3(int A[], int n){
-    int maxSum = maxSubArraySum(A,0,n);
-    return maxSum;
+ 
+/*Driver program to test maxSubArraySum*/
+float ThuatToan3(int A[], int n, int& maxSum){
+    clock_t start, end;
+    start = clock();
+    maxSum = maxSubArraySum(A,0,n-1);
+    end = clock();
+    return (float)(end-start)/CLOCKS_PER_SEC;
 }
-int ThuatToan4(int A[], int n){
-    int maxSum = 0;
+float ThuatToan4(int A[], int n, int &maxSum){
+    clock_t start, end;
+    start = clock();
     int sum = 0;
     for( int j = 0; j < n; j++ ) {
         sum += A[j];
@@ -79,48 +114,25 @@ int ThuatToan4(int A[], int n){
         else if (sum < 0)
             sum = 0;
     }
-    return maxSum;
+    end = clock();
+    return (float)(end-start)/CLOCKS_PER_SEC;
 } 
 int main()
 {
-    int maxSum[4] = {0,0,0,0};
     int A[MAXN];
-    float time[4] = {0,0,0,0};
-    int n =2000;
-    RandomArray(A,n);
-    clock_t start, end;
-    for(int i=1; i <= 4; i++){
-        start = 0;
-        end = 0;
-        if (i==1){
-            start = clock ();
-            maxSum[i] = ThuatToan1(A,n);
-            end = clock ();
-            time[i] = (float)(end-start)/CLOCKS_PER_SEC;
-        }
-        if (i==2){
-            start = clock ();
-            maxSum[i] = ThuatToan2(A,n);
-            end = clock ();
-            time[i] = (float)(end-start)/CLOCKS_PER_SEC;
-        }
-        if (i==3){
-            start = clock ();
-            time[i] = ThuatToan3(A,n);
-            end = clock ();
-            time[i] = (float)(end-start)/CLOCKS_PER_SEC;
-        }
-        if (i==4){
-            start = clock ();
-            maxSum[i] = ThuatToan4(A,n);
-            end = clock ();
-            time[i] = (float)(end-start)/CLOCKS_PER_SEC;
-        }        
-    }
-    cout<<"n = "<<n<<endl;
-    for(int i=1; i<=4;i++){
-        cout<<"Thoi gian chay thuat toan "<<i<<" = "<<setprecision(10)<<time[i]<<"\n    Ket qua = "<<maxSum[i]<<"\n";
-    }
-    
+    int testcase[] = {10, 100, 1000, 5000, 10000, 100000, 200000, 300000, 400000, 500000};
+	int n = 0;
+    do{
+        int maxSum1 = 0; int maxSum2 = 0; int maxSum3 = 0; int maxSum4 = 0;
+        RandomArray(A,testcase[n]);
+        cout<<"Truong hop n = "<<testcase[n]<<endl;
+        cout<<"\nThoi gian chay thuat toan 1: "<<setprecision(10)<<ThuatToan1(A,testcase[n],maxSum1)<<"\n    Ket qua = "<<maxSum1<<"\n";
+        cout<<"\nThoi gian chay thuat toan 2: "<<setprecision(10)<<ThuatToan2(A,testcase[n],maxSum2)<<"\n    Ket qua = "<<maxSum2<<"\n";
+        cout<<"\nThoi gian chay thuat toan 3: "<<setprecision(10)<<ThuatToan3(A,testcase[n],maxSum3)<<"\n    Ket qua = "<<maxSum3<<"\n";
+        cout<<"\nThoi gian chay thuat toan 4: "<<setprecision(10)<<ThuatToan4(A,testcase[n],maxSum4)<<"\n    Ket qua = "<<maxSum4<<"\n"
+            <<"-------------------------------------------------------------------------------------------\n"
+            <<endl;
+        n++;
+    } while (n <= 9);
     return 0;
 }
