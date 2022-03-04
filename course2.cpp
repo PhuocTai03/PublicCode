461f24d2847bc818179f62c7997225b252f90c0f
Dao Phuoc Tai <87965099+PhuocTai03@users.noreply.github.com>
2022-03-04 12:20:24 +0700

    Update course2.cpp

--- course2.cpp
+++ course2.cpp
@@ -11,10 +11,8 @@ void RandomArray(int A[], int n){
     for(int i=0; i < n; i++)
         A[i]=(rand()%100+1) - 50;
 }
-float ThuatToan1(int A[], int n, int& maxSum){
-    clock_t start, end;
-    start = clock();
-    maxSum = 0;
+int ThuatToan1(int A[], int n){
+    int maxSum = 0;
     int sum = 0;
     for (int i = 0; i < n; i ++) {
         for (int j = i; j < n; j ++) {
@@ -25,23 +23,18 @@ float ThuatToan1(int A[], int n, int& maxSum){
             maxSum = max(maxSum, sum);
         }
     }
-    end = clock();
-    return (float)(end-start)/CLOCKS_PER_SEC;
+    return maxSum;
 }
-float ThuatToan2(int A[], int n, int& maxSum){
-    clock_t start, end;
-    start = clock();
-    maxSum = 0;
-    int sum = 0;
+int ThuatToan2(int A[], int n){
+    int maxSum = 0;
     for( int i = 0; i < n; i++ ){
-        sum = 0;
+        int sum = 0;
         for( int j = i; j < n ; j++ ) {
             sum += A[j];
             maxSum = max(maxSum, sum);
         } 
     }
-    end = clock();
-    return (float)(end-start)/CLOCKS_PER_SEC;
+    return maxSum;
 }
 int maxCrossingSum(int arr[], int l, int mid, int r) {
     // Include e lements on left of mid.
@@ -72,17 +65,12 @@ int maxSubArraySum(int arr[], int l, int r) {
    return max(max(maxSubArraySum(arr, l, mid),maxSubArraySum(arr, mid+1, r)),
                             maxCrossingSum(arr, l, mid, r));
 }
-float ThuatToan3(int A[], int n, int& maxSum){
-    clock_t start, end;
-    start = clock();
-    maxSum = maxSubArraySum(A,0,n);
-    end = clock();
-    return (float)(end-start)/CLOCKS_PER_SEC;
+int ThuatToan3(int A[], int n){
+    int maxSum = maxSubArraySum(A,0,n);
+    return maxSum;
 }
-float ThuatToan4(int A[], int n, int &maxSum){
-    clock_t start, end;
-    start = clock();
-    maxSum = 0;
+int ThuatToan4(int A[], int n){
+    int maxSum = 0;
     int sum = 0;
     for( int j = 0; j < n; j++ ) {
         sum += A[j];
@@ -91,19 +79,48 @@ float ThuatToan4(int A[], int n, int &maxSum){
         else if (sum < 0)
             sum = 0;
     }
-    end = clock();
-    return (float)(end-start)/CLOCKS_PER_SEC;
+    return maxSum;
 } 
 int main()
 {
-    int maxSum1 = 0; int maxSum2 = 0; int maxSum3 = 0; int maxSum4 = 0;
+    int maxSum[4] = {0,0,0,0};
     int A[MAXN];
-    int n =1000;
+    float time[4] = {0,0,0,0};
+    int n =2000;
     RandomArray(A,n);
+    clock_t start, end;
+    for(int i=1; i <= 4; i++){
+        start = 0;
+        end = 0;
+        if (i==1){
+            start = clock ();
+            maxSum[i] = ThuatToan1(A,n);
+            end = clock ();
+            time[i] = (float)(end-start)/CLOCKS_PER_SEC;
+        }
+        if (i==2){
+            start = clock ();
+            maxSum[i] = ThuatToan2(A,n);
+            end = clock ();
+            time[i] = (float)(end-start)/CLOCKS_PER_SEC;
+        }
+        if (i==3){
+            start = clock ();
+            time[i] = ThuatToan3(A,n);
+            end = clock ();
+            time[i] = (float)(end-start)/CLOCKS_PER_SEC;
+        }
+        if (i==4){
+            start = clock ();
+            maxSum[i] = ThuatToan4(A,n);
+            end = clock ();
+            time[i] = (float)(end-start)/CLOCKS_PER_SEC;
+        }        
+    }
     cout<<"n = "<<n<<endl;
-    cout<<"Thoi gian chay thuat toan 1: "<<setprecision(10)<<ThuatToan1(A,n,maxSum1)<<"\n    Ket qua = "<<maxSum1<<"\n";
-    cout<<"\nThoi gian chay thuat toan 2: "<<setprecision(10)<<ThuatToan2(A,n,maxSum2)<<"\n    Ket qua = "<<maxSum2<<"\n";
-    cout<<"\nThoi gian chay thuat toan 3: "<<setprecision(10)<<ThuatToan3(A,n,maxSum3)<<"\n    Ket qua = "<<maxSum3<<"\n";
-    cout<<"\nThoi gian chay thuat toan 4: "<<setprecision(10)<<ThuatToan4(A,n,maxSum4)<<"\n    Ket qua = "<<maxSum4<<"\n";
+    for(int i=1; i<=4;i++){
+        cout<<"Thoi gian chay thuat toan "<<i<<" = "<<setprecision(10)<<time[i]<<"\n    Ket qua = "<<maxSum[i]<<"\n";
+    }
+    
     return 0;
 }
